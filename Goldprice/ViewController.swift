84a4292a0bc8e5789d8eef514b26894480482d6d
JamesNguyen <ntkhoiptit@gmail.com>
2017-06-11 14:17:18 +0700

    Init project . Ignore storyboard

---
+++ Goldprice/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Goldprice
+//
+//  Created by Nguyen Trong Khoi on 6/11/17.
+//  Copyright © 2017 Nguyen Trong Khoi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+
+
+}
+
